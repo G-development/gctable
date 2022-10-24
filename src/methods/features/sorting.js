41cb06f018956fa1f93e2641d532d33fbb0e9d99
Cuozzo, Giovanni <giovanni.cuozzo@nttdata.com>
2022-10-24 09:43:26 +0200

    +Sorting w/ matchSorter

---
+++ src/methods/features/sorting.js
@@ -0,0 +1,7 @@
+import { matchSorter } from "match-sorter";
+
+export function matchSorterFn(rows, id, filterValue) {
+  return matchSorter(rows, filterValue, {
+    keys: [(row) => row.values[id].value],
+  });
+}
