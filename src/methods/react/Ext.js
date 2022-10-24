54f992ff7257f6e641eca43c45af0a0ed7efc95b
Cuozzo, Giovanni <giovanni.cuozzo@nttdata.com>
2022-10-24 09:43:47 +0200

    Clear Ext.js

--- src/methods/react/Ext.js
+++ src/methods/react/Ext.js
@@ -4,12 +4,7 @@ import { returnData } from "../features/handleData";
 
 const Ext = ({ layout }) => {
   var { data, headers } = returnData(layout);
-  return (
-    <>
-      {/* <h1 style={{ textAlign: "center" }}>Vis: {layout.visualization}</h1> */}
-      <Table tableData={data} headers={headers} gct={layout.GCT} />
-    </>
-  );
+  return <Table tableData={data} headers={headers} gct={layout.GCT} />;
 };
 
 export default Ext;
