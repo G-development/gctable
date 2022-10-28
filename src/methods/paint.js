82d28fe61c7c9a1ba26c90ed3d56b13b8b5925e7
Cuozzo, Giovanni <giovanni.cuozzo@nttdata.com>
2022-10-28 16:03:43 +0200

    +Fetch all data (backendApi)

--- src/methods/paint.js
+++ src/methods/paint.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
 import Ext from "./react/Ext";
 
 import "./style.css";
@@ -8,6 +7,26 @@ import "./style.css";
 export var qlik = window.require("qlik");
 
 export default function paint($element, layout) {
+  // console.log("Paint", layout);
+  var lastrow = 0,
+    me = this;
+
+  this.backendApi.eachDataRow((rownum) => (lastrow = rownum));
+
+  if (this.backendApi.getRowCount() > lastrow + 1) {
+    var requestPage = [
+      {
+        qTop: lastrow + 1,
+        qLeft: 0,
+        qWidth: 40,
+        qHeight: Math.min(1000, this.backendApi.getRowCount() - lastrow),
+      },
+    ];
+    this.backendApi.getData(requestPage).then(function (dataPages) {
+      me.paint($element, layout);
+    });
+  }
+
   if (!this.$scope.root) {
     const container = document.getElementById("GC-Table_" + layout.qInfo.qId);
     const root = createRoot(container);
